185485eaf39d341948da2995ff335f3d989b610c
Son Luong Ngoc <sluongng@gmail.com>
2023-12-01 09:28:55 +0100

    day1: second star

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,14 +1,47 @@
 fn main() {
+    let digi_words = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
     let input = "
-1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
 ";
 
     let answer = input
         .trim()
         .split("\n")
+        .map(|s| {
+            let mut line = s.to_string();
+
+            loop {
+                let mut min_index = usize::MAX;
+                let mut first_word = "";
+                let mut replace = 0;
+
+                for (i, word) in digi_words.iter().enumerate() {
+                    if let Some(index) = line.find(word) {
+                        if index < min_index {
+                            println!("{s}: found {word} at {index}");
+                            min_index = index;
+                            first_word = word;
+                            replace = i + 1;
+                        }
+                    }
+                }
+                if first_word == "" {
+                    break;
+                }
+
+                line = line.replace(first_word, &replace.to_string());
+            }
+
+            line
+        })
         .map(|s| {
             let mut iter = s.chars().filter(|c| c.is_numeric());
 
@@ -19,10 +52,10 @@ treb7uchet
             };
 
             let line = first * 10 + last;
-            // println!("{line}");
+            println!("{s}: {line}");
             line
         })
         .sum::<u32>();
 
-    println!("{answer}");
+    println!("Total: {answer}");
 }
